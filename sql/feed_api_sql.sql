5ec6e05b20024716ab19b465e843d9b4250e3825
vishal13may <vishal13may@gmail.com>
2017-08-25 20:37:20 -0700

    Added Sql

---
+++ sql/feed_api_sql.sql
@@ -0,0 +1,22 @@
+create table user (id int AUTO_INCREMENT, name varchar(50), emailId varchar(50), PRIMARY KEY (id));
+
+create table feed (id int AUTO_INCREMENT, name varchar(50), PRIMARY KEY(id));
+
+create table subscription (id int AUTO_INCREMENT, feedId int, userId int, startDate DATE, endDate DATE,  isActive boolean, PRIMARY KEY(feedId, userId), KEY(id));
+
+create table article (id int AUTO_INCREMENT, title varchar(255), content text, author varchar(50), PRIMARY KEY(id));
+
+create table feed_article (feedId int, articleId int);
+
+
+insert into user (name, emailId) values('Vishal', 'vishalos@usc.edu');
+insert into user (name, emailId) values('Albert', 'albert@usc.edu');
+insert into user (name, emailId) values('Gary', 'gary@usc.edu');
+
+insert into feed (name) values('Technology');
+insert into feed (name) values('Arts');
+insert into feed (name) values('Economics');
+insert into feed (name) values('History');
+insert into feed (name) values('Current Affairs');
+
+
